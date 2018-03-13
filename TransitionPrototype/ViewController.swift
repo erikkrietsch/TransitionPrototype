609ab389595cd67a4a8e70e98f2e16c6c4f77d62
Erik Krietsch <erik.krietsch@gmail.com>
2018-03-13 14:43:58 -0500

    Initial Commit

---
+++ TransitionPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionPrototype
+//
+//  Created by Erik Krietsch on 3/13/18.
+//  Copyright © 2018 Totally Radical Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
